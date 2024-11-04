edeb43a50f899b7545e96e721274816ace152acc
Clafer03 <claudi125.lol@gmail.com>
2024-11-04 18:16:24 -0500

    error en main.js corregido

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import '@/assets/css/main.css'
-import '/dist/output.css'
+import './input.css'
 
 createApp(App)
     .use(store)
