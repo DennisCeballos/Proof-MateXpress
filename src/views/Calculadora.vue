2538d8f6ec6b811f8cfbeb17ee57082df8d856ac
Clafer03 <claudi125.lol@gmail.com>
2024-11-29 12:15:20 -0500

    Update Calculadora.vue

--- src/views/Calculadora.vue
+++ src/views/Calculadora.vue
@@ -231,10 +231,9 @@
                   type="number"
                   v-model.number="rowsB"
                   min="1"
-                  max="10"
-                  :disabled="operation === 'add' || operation === 'subtract' || operation === 'multiply'"
+                  max="10" 
                   class="w-20 p-1 border rounded"
-                />
+                /> <!--:disabled="operation === 'add' || operation === 'subtract' || operation === 'multiply'"-->
               </div>
               <div>
                 <label class="block text-sm">Columnas:</label>
@@ -243,9 +242,8 @@
                   v-model.number="colsB"
                   min="1"
                   max="10"
-                  :disabled="operation === 'add' || operation === 'subtract'"
                   class="w-20 p-1 border rounded"
-                />
+                /> <!--:disabled="operation === 'add' || operation === 'subtract'"-->
               </div>
             </div>
             <MatrixInput
